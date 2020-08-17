c541e8cbb9635adde1a0c1154fbc94b9a87c7d34
J. Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2020-08-17 15:17:01 -0500

    Handling force unwrapping.

--- Tests/FeedStoreChallengeTests.swift
+++ Tests/FeedStoreChallengeTests.swift
@@ -44,12 +44,19 @@ class RealmFeedStore: FeedStore {
         realm = try Realm()
     }
     
+    enum Error: Swift.Error {
+        case invalidData
+    }
+    
     func retrieve(completion: @escaping RetrievalCompletion) {
         let feedCached = realm.objects(RealmCache.self).first
         if let cache = feedCached, cache.feed.count != 0 {
             var localFeed = [LocalFeedImage]()
             cache.feed.forEach { image in
-                localFeed.append(LocalFeedImage(id: UUID(uuidString: image.id)!, description: image.desc, location: image.location, url: URL(string: image.url)!))
+                guard let uuid = UUID(uuidString: image.id), let url = URL(string: image.url) else {
+                    return completion(.failure(Error.invalidData))
+                }
+                localFeed.append(LocalFeedImage(id: uuid, description: image.desc, location: image.location, url: url))
             }
             completion(.found(feed: localFeed, timestamp: cache.timestamp))
         } else {
@@ -58,22 +65,30 @@ class RealmFeedStore: FeedStore {
     }
     
     func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-        try! realm.write {
-            realm.deleteAll()
-            let realmFeed = List<RealmFeedImage>()
-            feed.map { RealmFeedImage(id: $0.id.uuidString, desc: $0.description, location: $0.location, url: $0.url.absoluteString) }.forEach {
-                realmFeed.append($0)
+        do {
+            try realm.write {
+                realm.deleteAll()
+                let realmFeed = List<RealmFeedImage>()
+                feed.map { RealmFeedImage(id: $0.id.uuidString, desc: $0.description, location: $0.location, url: $0.url.absoluteString) }.forEach {
+                    realmFeed.append($0)
+                }
+                let cache = RealmCache(feed: realmFeed, timestamp: timestamp)
+                realm.add(cache)
+                completion(nil)
             }
-            let cache = RealmCache(feed: realmFeed, timestamp: timestamp)
-            realm.add(cache)
-            completion(nil)
+        } catch {
+            completion(error)
         }
     }
     
     func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-        try! realm.write {
-            realm.deleteAll()
-            completion(nil)
+        do {
+            try realm.write {
+                realm.deleteAll()
+                completion(nil)
+            }
+        }catch {
+            completion(error)
         }
     }
 }
