c840446975510e842cf36830dfb4e235eb7223f2
J. Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2020-08-26 11:26:41 -0500

    Separates `insert` and `delete` operations into helper methods.

--- Tests/FeedStoreChallengeIntegrationTests.swift
+++ Tests/FeedStoreChallengeIntegrationTests.swift
@@ -3,47 +3,68 @@
 //
 
 import XCTest
+import RealmSwift
 import FeedStoreChallenge
 
 class FeedStoreChallengeIntegrationTests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+        Realm.Configuration.defaultConfiguration.inMemoryIdentifier = self.name
+    }
+    
     func test_retrieve_deliversItemsSavedOnASeparateInstance() {
-        let sutToPerformSave = try! RealmFeedStore()
-        let sutToPerformLoad = try! RealmFeedStore()
+        let sutToPerformSave = makeSUT()
+        let sutToPerformLoad = makeSUT()
         
-        let exp = expectation(description: "Wait for insertion.")
-        let localFeed = uniqueLocalImageFeed()
-        let timestamp = Date()
-        sutToPerformSave.insert(localFeed, timestamp: timestamp) { error in
-            XCTAssertNil(error, "Expected insert images. Got \(String(describing: error)) instead.")
-            exp.fulfill()
-        }
-        wait(for: [exp], timeout: 1.0)
+        let cache = expectedCache()
+        insert(feed: cache.feed, timestamp: cache.timestamp, on: sutToPerformSave)
         
-        let exp2 = expectation(description: "Wait to retrieve")
-        sutToPerformLoad.retrieve { result in
-            switch result {
-            case let .found(retrievedFeed, retrievedTimestamp):
-                XCTAssertEqual(retrievedFeed, localFeed)
-                XCTAssertEqual(retrievedTimestamp, timestamp)
-            default:
-                XCTFail("Expected to found feed. Got \(result) instead.")
-            }
-            exp2.fulfill()
-        }
-        wait(for: [exp2], timeout: 1.0)
+        expect(sutToPerformLoad, toLoad: cache.feed, timestamp: cache.timestamp)
     }
     
     // MARK: - Helpers
     
-    func uniqueLocalImageFeed() -> [LocalFeedImage] {
-        [uniqueLocalImage(), uniqueLocalImage()]
+    private func makeSUT() -> RealmFeedStore {
+        try! RealmFeedStore()
     }
     
-    func uniqueLocalImage(description: String? = nil, location: String? = nil) -> LocalFeedImage {
+    private func insert(feed: [LocalFeedImage], timestamp: Date, on sut: RealmFeedStore) {
+        let exp = expectation(description: "Wait for insertion.")
+        sut.insert(feed, timestamp: timestamp) { error in
+            XCTAssertNil(error, "Expected insert images. Got \(String(describing: error)) instead.")
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+    private func expect(_ sut: RealmFeedStore, toLoad expectedFeed: [LocalFeedImage], timestamp expectedTimestamp: Date) {
+        let exp = expectation(description: "Wait to retrieve")
+        sut.retrieve { result in
+            switch result {
+            case let .found(retrievedFeed, retrievedTimestamp):
+                XCTAssertEqual(retrievedFeed, expectedFeed)
+                XCTAssertEqual(retrievedTimestamp, expectedTimestamp)
+            default:
+                XCTFail("Expected to found feed. Got \(result) instead.")
+            }
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+    private func expectedCache() -> (feed: [LocalFeedImage], timestamp: Date) {
+        return (uniqueLocalImageFeed(), Date())
+    }
+    
+    private func uniqueLocalImageFeed() -> [LocalFeedImage] {
+        return [uniqueLocalImage(), uniqueLocalImage()]
+    }
+    
+    private func uniqueLocalImage(description: String? = nil, location: String? = nil) -> LocalFeedImage {
         LocalFeedImage(id: UUID(), description: description, location: location, url: anyURL())
     }
     
-    func anyURL() -> URL {
+    private func anyURL() -> URL {
         URL(string: "http://any-url.com")!
     }
 }
